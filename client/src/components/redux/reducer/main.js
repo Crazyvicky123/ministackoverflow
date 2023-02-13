80cbcd417a250227c7e83618cec1491e79943e1a
Vicky the Designer <94231823+Crazyvicky123@users.noreply.github.com>
2023-02-13 11:43:49 -0500

    Delete client directory

--- client/src/components/redux/reducer/main.js
+++
@@ -1,9 +0,0 @@
-import {getQuestionreducer} from "./Questionreducer"
-import {combineReducers} from "redux";
-
-const rootreducers = combineReducers({
-    getquestiondata:getQuestionreducer
-});
-
-
-export default rootreducers;
